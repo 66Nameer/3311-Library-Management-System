8c21e14fe01d8a296843071cd9cf17653bfbe791
Ismail M. Al Amassi <77177926+Amassi-Ismail@users.noreply.github.com>
2024-03-08 18:57:04 -0500

    Create Cart.java

---
+++ src/main/java/Cart.java
@@ -0,0 +1,26 @@
+public class Cart {
+	
+private HashMap<Item, Integer> items; 
+
+	public void addItem(int itemId, int quantity) {
+	
+	
+}
+	
+	public void removeItem(int itemId, int quantity) {
+		
+		
+		
+	}
+	
+	public int getItemQuantity(int itemId) {
+		
+		return 0;
+	}
+	
+	public void displayCart() {
+		
+		
+	}
+	
+}
