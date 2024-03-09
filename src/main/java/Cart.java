958ce821d480a13dbde45605f6b9a5dee7cc9151
Ismail Alamassi <ismailamassi2002@gmail.com>
2024-03-08 20:22:46 -0500

    update

--- src/main/java/Cart.java
+++ src/main/java/Cart.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 
 public class Cart {
 	
-private HashMap<Item, Integer> items1;
+private HashMap<Item, Integer> items11;
 
 	public void addItem(int itemId, int quantity) {
 	
