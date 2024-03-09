b1d1da70b9685b6e3c1cb197d7c7f0a15c18a678
HuyAnhVuTran <91705590+HuyAnhVuTran@users.noreply.github.com>
2024-03-08 19:08:20 -0500

    Add files via upload

---
+++ src/main/java/Manager.java
@@ -0,0 +1,31 @@
+package user;
+
+import Item.Item;
+
+public class Manager extends User{
+
+	
+	public void AddItem (Item item) {
+		
+	}
+	
+	public void EnableItemForRent(int itemID) {
+		
+	}
+	
+	public void EditItemDetails(int item, String updatedDetails) {
+		
+	}
+	
+	public void DisableItemForRent(int itemID) {
+		
+	}
+	
+	public void ValidateRegistration() {
+		
+	}
+	
+	public void DisplayNotification(String message) {
+		
+	}
+}
