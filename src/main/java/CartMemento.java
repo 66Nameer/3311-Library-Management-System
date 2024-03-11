4900319296dd998f6293e3dd6d4d29926191eba2
HuyAnhVuTran <91705590+HuyAnhVuTran@users.noreply.github.com>
2024-03-11 18:33:40 -0400

    Update CartMemento.java

--- src/main/java/CartMemento.java
+++ src/main/java/CartMemento.java
@@ -7,10 +7,13 @@ public class CartMemento {
     private HashMap<Item, Integer> items;
 
     public CartMemento(HashMap<Item, Integer> items) {
-        this.items = new HashMap<Item, Integer>(items);
+        this.items = new HashMap<Item, Integer>(items.size());
+        for (Map.Entry<Item, Integer> entry : items.entrySet()) {
+            this.items.put(entry.getKey(), entry.getValue());
+        }
     }
 
     public HashMap<Item, Integer> getItems(){
-        return items;
+        return this.items;
     }
 }
