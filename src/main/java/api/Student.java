2ddbc8f87a5102de8f381c2c7a0a0eec8d6f315a
Divinity <69832329+Divine144@users.noreply.github.com>
2024-03-19 16:43:15 -0400

    Implemented some database methods

--- src/main/java/api/Student.java
+++ src/main/java/api/Student.java
@@ -11,4 +11,8 @@ public class Student extends User {
 	private boolean enrolled =false;
 	private ArrayList<Date> examDates=new ArrayList<Date>();
 	private Map<String , String > courses=new HashMap<String, String>();
+
+	public Student(String email, String password) {
+		super(email, password);
+	}
 }
