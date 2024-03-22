356af7a18ffb0c804e6268161661076ef8114855
HuyAnhVuTran <91705590+HuyAnhVuTran@users.noreply.github.com>
2024-03-21 23:24:14 -0400

    Update MainFrame.java

--- src/main/java/gui/MainFrame.java
+++ src/main/java/gui/MainFrame.java
@@ -18,23 +18,23 @@ public class MainFrame extends JFrame {
         cardLayout = new CardLayout();
         cardPanel = new JPanel(cardLayout);
 
-        ImageIcon icon = new ImageIcon("src/main/java/gui/icon1.png"); // Replace "path_to_your_icon_image.png" with the path to your icon image file
+        ImageIcon icon = new ImageIcon("src/main/java/gui/icon1.png"); 
         setIconImage(icon.getImage());
 
 
-        // Initialize the LoginPanel and add it to cardPanel with an identifier
-        LoginPanel loginPanel = new LoginPanel(this); // Pass reference to MainFrame
+       
+        LoginPanel loginPanel = new LoginPanel(this); 
         cardPanel.add(loginPanel, "LoginPanel");
 
-        // Initialize the RegisterPanel and add it to cardPanel with an identifier
-        RegisterPanel registerPanel = new RegisterPanel(this); // Pass reference to MainFrame
+        
+        RegisterPanel registerPanel = new RegisterPanel(this); 
         cardPanel.add(registerPanel, "RegisterPanel");
 
-        // Initialize the UserDashboardPanel and add it to cardPanel with an identifier
+   
         UserDashboardPanel userDashboardPanel = new UserDashboardPanel(this);
         cardPanel.add(userDashboardPanel, "UserDashboardPanel");
 
-        // Initialize the BrowsingPanel and add it to cardPanel with an identifier
+      
         BrowsingPanel browsingPanel = new BrowsingPanel();
         cardPanel.add(browsingPanel, "BrowsingPanel");
 
@@ -43,20 +43,19 @@ public class MainFrame extends JFrame {
         cardPanel.add(subscribePanel, "SubscribePanel");
 
         NewsletterBrowserPanel browserPanel = new NewsletterBrowserPanel();
-        browserPanel.loadURL("https://www.nytimes.com"); // Example URL
         cardPanel.add(browserPanel, "NewsBrowser");
 
         
-        // Set up the frame
+       
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(new Dimension(800, 600));
         this.add(cardPanel);
 
-        // Show the initial card (the login panel)
+       
         cardLayout.show(cardPanel, "LoginPanel");
     }
 
-    // Methods to switch cards, if necessary
+  
     public void showCard(String card) {
         cardLayout.show(cardPanel, card);
     }
