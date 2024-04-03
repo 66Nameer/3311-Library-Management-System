71df3dc50790c81ff1d450dad549a5bf4bf0d156
HuyAnhVuTran <91705590+HuyAnhVuTran@users.noreply.github.com>
2024-04-03 12:32:38 -0400

    Update Student.java

--- src/main/java/api/Student.java
+++ src/main/java/api/Student.java
@@ -15,4 +15,20 @@ public class Student extends User {
 	public Student(String email, String password) {
 		super(email, password, UserType.STUDENT);
 	}
+
+	public boolean getEnrolled(){
+		return this.enrolled;
+	}
+
+	public ArrayList<Date> getExamDates(){
+		return this.examDates;
+	}
+
+	public Map<String,String> getCourses(){
+		return this.courses;
+	}
+
+	public void setEnrolled(boolean enrolled){
+		this.enrolled=enrolled;
+	}
 }
